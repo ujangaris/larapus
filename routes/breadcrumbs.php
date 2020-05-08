c4dc9de83a47b8b5c5bca7bd325182d404c12386
ujangaris <ujangaja@gmail.com>
2020-05-09 06:02:47 +0700

    menyisipkan featur penulis

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -14,11 +14,13 @@ Breadcrumbs::for('admin.author.index', function ($trail) {
 // Author Add
 Breadcrumbs::for('admin.author.create', function ($trail) {
     $trail->push('Beranda', route('admin.dashboard'));
+    $trail->push('Penulis', route('admin.author.index'));
     $trail->push('Tambah Penulis', route('admin.author.create'));
 });
 
 // Author Edit
 Breadcrumbs::for('admin.author.edit', function ($trail, $author) {
     $trail->push('Beranda', route('admin.dashboard'));
+    $trail->push('Penulis', route('admin.author.index'));
     $trail->push('Edit Penulis', route('admin.author.edit', $author));
 });
