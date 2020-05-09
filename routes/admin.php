c1811e7eab800d3eb359fe65911d99f01cfc3c7d
ujangaris <ujangaja@gmail.com>
2020-05-09 14:32:20 +0700

    menampilkan data json dari books

--- routes/admin.php
+++ routes/admin.php
@@ -2,5 +2,6 @@
 Route::get('/', 'HomeController@index')->name('dashboard');
 
 Route::get('/author/data', 'DataController@authors')->name('author.data');
+Route::get('/book/data', 'DataController@books')->name('book.data');
 //wajib taruh dibawah DataController
 Route::resource('author', 'AuthorCOntroller');
