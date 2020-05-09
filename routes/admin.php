3d151b4d54c57fc014e4bc75f498ff546c32edfe
ujangaris <ujangaja@gmail.com>
2020-05-09 13:32:36 +0700

    menyederhanakan routing dengan resource

--- routes/admin.php
+++ routes/admin.php
@@ -1,12 +1,6 @@
 <?php
 Route::get('/', 'HomeController@index')->name('dashboard');
 
-Route::get('/author', 'AuthorController@index')->name('author.index');
-Route::get('/author/create', 'AuthorController@create')->name('author.create');
-Route::post('/author', 'AuthorController@store')->name('author.store');
-Route::get('/author/{author}/edit', 'AuthorController@edit')->name('author.edit');
-Route::put('/author/{author}', 'AuthorController@update')->name('author.update');
-Route::delete('/author/{author}', 'AuthorController@destroy')->name('author.destroy');
-
-
 Route::get('/author/data', 'DataController@authors')->name('author.data');
+//wajib taruh dibawah DataController
+Route::resource('author', 'AuthorCOntroller');
