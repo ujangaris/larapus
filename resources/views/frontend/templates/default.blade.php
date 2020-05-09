c01983e0d28c31b3aec21b7a895a0c2c92b94f79
ujangaris <ujangaja@gmail.com>
2020-05-10 04:25:59 +0700

    membuat template halaman depan

---
+++ resources/views/frontend/templates/default.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+@include('frontend.templates.partials.head')
+  <body>
+      {{-- Navigation --}}
+    @include('frontend.templates.partials.navigation')
+
+
+
+      {{-- Content --}}
+    <div class="container">
+        @yield('content')
+    </div>
+    <!-- Optional JavaScript -->
+    @include('frontend.templates.partials.script')
+  </body>
+</html>
---
+++ resources/views/frontend/templates/partials/head.blade.php
@@ -0,0 +1,11 @@
+<head>
+    <title>Home Page</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Compiled and minified CSS Materialize -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+</head>
