250d013214a1a37869cdf2178fbb0243df2866da
ujangaris <ujangaja@gmail.com>
2020-05-10 06:16:45 +0700

    membuat tampilan register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,41 @@
-@extends('layouts.app')
+@extends('frontend.templates.default')
 
 @section('content')
-<div class="container">
+    <div class="container">
+        <h3>Register</h3>
+        <form action="{{ route('register') }}" class="col s12" method="post">
+            @csrf
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <i class="material-icons prefix">person</i>
+                    <input type="text" name="name" value="{{ old('name') }}">
+                    <label for="">Name</label>
+                </div>
+                <div class="input-field col s12">
+                    <i class="material-icons prefix">email</i>
+                    <input type="email" name="email" value="{{ old('email') }}">
+                    <label for="">Email</label>
+                </div>
+                <div class="input-field col s12">
+                    <i class="material-icons prefix">lock</i>
+                    <input type="password" name="password">
+                    <label for="">Password</label>
+                </div>
+                <div class="input-field col s12">
+                    <i class="material-icons prefix">lock</i>
+                    <input type="password" name="password_convirmation">
+                    <label for="">Password Confirmation</label>
+                </div>
+                <div class="input-field right">
+                    <input type="submit" value="Register" class="vawes-effect waves-light btn red accent-1">
+                </div>
+            </div>
+        </form>
+    </div>
+@endsection
+
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -73,5 +107,4 @@
             </div>
         </div>
     </div>
-</div>
-@endsection
+</div> --}}
