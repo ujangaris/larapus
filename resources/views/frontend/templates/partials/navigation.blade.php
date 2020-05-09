6504bd2a65937ece66b4269f9f62ef8fa4d6b379
ujangaris <ujangaja@gmail.com>
2020-05-10 05:10:40 +0700

    navbar-fixed

--- resources/views/frontend/templates/partials/navigation.blade.php
+++ resources/views/frontend/templates/partials/navigation.blade.php
@@ -1,13 +1,15 @@
-<nav>
-    <div class="nav-wrapper">
-      <a href="#!" class="brand-logo">Perpusku</a>
-      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-      <ul class="right hide-on-med-and-down">
-        <li><a href="#">Login</a></li>
-        <li><a href="#">Register</a></li>
-       </ul>
-    </div>
-</nav>
+<div class="navbar-fixed">
+    <nav>
+        <div class="nav-wrapper">
+          <a href="#!" class="brand-logo">Perpusku</a>
+          <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+          <ul class="right hide-on-med-and-down">
+            <li><a href="#">Login</a></li>
+            <li><a href="#">Register</a></li>
+           </ul>
+        </div>
+    </nav>
+</div>
 
   <ul class="sidenav" id="mobile-demo">
     <li><a href="#">Login</a></li>
