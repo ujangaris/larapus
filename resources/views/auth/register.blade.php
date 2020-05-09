38e79e534c3003fa1ed5da7cf1a2d177e64badc2
ujangaris <ujangaja@gmail.com>
2020-05-10 06:31:18 +0700

    pesan error

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,23 +9,35 @@
             <div class="row">
                 <div class="input-field col s12">
                     <i class="material-icons prefix">person</i>
-                    <input type="text" name="name" value="{{ old('name') }}">
+                    <input type="text" name="name" class="@error('name') invalid @enderror" value="{{ old('name') }}">
                     <label for="">Name</label>
+                    @error('name')
+                        <span class="helper-text" data-error="{{ $message }}"></span>
+                    @enderror
                 </div>
                 <div class="input-field col s12">
                     <i class="material-icons prefix">email</i>
-                    <input type="email" name="email" value="{{ old('email') }}">
+                    <input type="email" name="email" class="validate @error('email') invalid @enderror" value="{{ old('email') }}">
                     <label for="">Email</label>
+                    @error('email')
+                        <span class="helper-text" data-error="{{ $message }}"></span>
+                    @enderror
                 </div>
                 <div class="input-field col s12">
                     <i class="material-icons prefix">lock</i>
-                    <input type="password" name="password">
+                    <input type="password" name="password" class="@error('password') invalid @enderror">
                     <label for="">Password</label>
+                    @error('password')
+                        <span class="helper-text" data-error="{{ $message }}"></span>
+                    @enderror
                 </div>
                 <div class="input-field col s12">
                     <i class="material-icons prefix">lock</i>
-                    <input type="password" name="password_convirmation">
+                    <input type="password" name="password_confirmation" class="@error('password_confirmation') invalid @enderror">
                     <label for="">Password Confirmation</label>
+                    @error('password_confirmation')
+                        <span class="helper-text" data-error="{{ $message }}"></span>
+                    @enderror
                 </div>
                 <div class="input-field right">
                     <input type="submit" value="Register" class="vawes-effect waves-light btn red accent-1">
