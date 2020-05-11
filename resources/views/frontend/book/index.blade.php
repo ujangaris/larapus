081cc6567577e364e6567937bf1dfaa423f7445b
ujangaris <ujangaja@gmail.com>
2020-05-11 11:25:51 +0700

    membuat komponen card buku

--- resources/views/frontend/book/index.blade.php
+++ resources/views/frontend/book/index.blade.php
@@ -7,28 +7,9 @@
     </blockquote>
 <div class="row">
     @foreach($books as $book)
-        <div class="col s12 m6">
-            <div class="card horizontal hoverable">
-                <div class="card-image">
-                <img src="{{ $book->getCover() }}" height="200px">
-                </div>
-                <div class="card-stacked">
-                <div class="card-content">
-                    <h6>
-                        <a href="{{ route('book.show', $book) }}">{{ Str::limit($book->title, 20) }}</a>
-                    </h6>
-                    <p>{{ Str::limit($book->description, 50) }}</p>
-                </div>
-                <div class="card-action">
-
-                    <form action="{{ route('book.borrow', $book) }}" method="post">
-                        @csrf
-                        <input type="submit" value="Pinjam Buku" class="btn red accent-1 right waves-effect waves-light">
-                    </form>
-                </div>
-                </div>
-            </div>
-        </div>
+    @include('frontend.templates.components.card-book', [
+        'book' =>$book
+    ])
     @endforeach
 </div>
 
