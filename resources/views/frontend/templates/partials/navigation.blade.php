2d6cf5deee20bc782abf0e8a1393dd37e1a25130
ujangaris <ujangaja@gmail.com>
2020-05-11 10:02:12 +0700

    membenahi navigasi dan menambahkan fungsi logout

--- resources/views/frontend/templates/partials/navigation.blade.php
+++ resources/views/frontend/templates/partials/navigation.blade.php
@@ -4,14 +4,39 @@
           <a href="{{ route('homepage') }}" class="brand-logo">Perpusku</a>
           <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
           <ul class="right hide-on-med-and-down">
+            @guest
             <li><a href="{{ route('login') }}">Login</a></li>
             <li><a href="{{ route('register') }}">Register</a></li>
+            @else
+            <ul id="dropdown1" class="dropdown-content">
+                <li><a href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                                  document.getElementById('logout-form').submit();">
+                     {{ __('Logout') }}</a></li>
+            </ul>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+            <li><a href="#" class="dropdown-trigger" data-target="dropdown1">{{ auth()->user()->name }}</a></li>
+            @endguest
            </ul>
         </div>
     </nav>
 </div>
 
   <ul class="sidenav" id="mobile-demo">
+    @guest
     <li><a href="{{ route('login') }}">Login</a></li>
     <li><a href="{{ route('register') }}">Register</a></li>
+    @else
+    <li><a href="{{ route('logout') }}"
+        onclick="event.preventDefault();
+                      document.getElementById('logout-form').submit();">
+         {{ __('Logout') }}</a>
+    </li>
+    @endguest
   </ul>
+
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+         @csrf
+    </form>
